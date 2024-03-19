88f16b141ad6ef23fa27d5a04db53b43cc8820c0
jeeivan <jeeivansri@gmail.com>
2024-03-19 11:49:50 +0000

    Commit before merge

--- database.js
+++ database.js
@@ -1,11 +1,11 @@
 const { Client } = require('pg');
 
 const dbConfig = {
-  user: process.env.PGUSER,
-  host: process.env.PGHOST,
-  database: process.env.PGDATABASE,
-  password: process.env.PGPASSWORD, 
-  port: process.env.PGPORT, 
+  user: 'postgres',
+  host: 'monorail.proxy.rlwy.net',
+  database: 'railway',
+  password: 'SsOEKWqkEnvsfCtdvvUgnUcWzBZQBqoV',
+  port: 59815,
 };
 
 const client = new Client(dbConfig);
