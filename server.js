024d0f7ac022607ed13a1518292cca0b67dae536
OnlyOneLz <elliotrobertlewis@hotmail.com>
2024-07-21 12:38:19 +0100

    update host

--- server.js
+++ server.js
@@ -17,4 +17,6 @@ app.use("/answer", answerRoutes);
 app.use("/scoreboard", scoreboardRoutes);
 
 const PORT = process.env.PORT || 4001;
-app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
+app.listen(PORT, "0.0.0.0", () =>
+  console.log(`Server listening on port ${PORT}`)
+);
