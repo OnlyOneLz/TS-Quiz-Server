77567b953de29004d8cef53d109338ab5a2230b0
OnlyOneLz <elliotrobertlewis@hotmail.com>
2024-07-21 12:29:45 +0100

    updated port

--- server.js
+++ server.js
@@ -1,19 +1,20 @@
-const express = require("express")
-const cors = require("cors")
-const userRoutes = require('./routes/userRoutes');
-const questionRoutes = require('./routes/questionRoutes')
-const answerRoutes = require('./routes/answerRoutes')
-const scoreboardRoutes = require('./routes/scoreboardRoutes')
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+const userRoutes = require("./routes/userRoutes");
+const questionRoutes = require("./routes/questionRoutes");
+const answerRoutes = require("./routes/answerRoutes");
+const scoreboardRoutes = require("./routes/scoreboardRoutes");
+require("dotenv").config();
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
-app.use('/user', userRoutes);
-app.use('/question', questionRoutes);
-app.use('/answer', answerRoutes);
-app.use('/scoreboard', scoreboardRoutes);
+app.use("/user", userRoutes);
+app.use("/question", questionRoutes);
+app.use("/answer", answerRoutes);
+app.use("/scoreboard", scoreboardRoutes);
 
-app.listen(4001, () => console.log("Server listening on port 4001"))
+const PORT = process.env.PORT || 4001;
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
